7a74c21dcc8b8fb4a8d75287cb7771a9e87e6e41
Ronan Gray <echosounds998@gmail.com>
2022-11-02 10:14:59 +0800

    Added QTE sequence / organized and fixed

---
+++ Assets/MicroGames/QTE Sequence/ButtonSequence.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class ButtonSequence : MonoBehaviour
+{
+    public PlayerInputs inputs;
+
+    [SerializeField] private int sequenceLength, numberOfLoops;
+    private int currKeyNum = 0;
+    [SerializeField] private float waitTime = 1f;
+
+    [SerializeField] private string inputValue, desiredValue;
+    private bool canInput = true, playing = true;
+    [SerializeField] private bool looping;
+
+
+    [SerializeField] private List<string> buttonSequence = new List<string>();
+    private List<string> possibleInputs = new List<string>();
+
+    void Awake()
+    {
+        inputs = new PlayerInputs();
+
+    }
+    private void Start()
+    {
+        inputs.ButtonSequenceInputs.Enable();
+
+        Initialise();
+        GenerateSequence();
+    }
+
+    private void Update()
+    {
+        PressButtonCheck();
+    }
+    private void Initialise()
+    {
+        possibleInputs.Add("Q");
+        possibleInputs.Add("W");
+        possibleInputs.Add("E");
+        possibleInputs.Add("A");
+        possibleInputs.Add("S");
+        possibleInputs.Add("D");
+    }
+    private void GenerateSequence()
+    {
+        buttonSequence = new List<string>();
+        currKeyNum = 0;
+
+        int valueToAdd = Random.Range(0, possibleInputs.Count);
+        buttonSequence.Add(possibleInputs[valueToAdd]);
+
+        desiredValue = buttonSequence[0];
+
+        for (int i = 1; i < sequenceLength; i++)
+        {
+            valueToAdd = Random.Range(0, possibleInputs.Count);
+
+            if (possibleInputs[valueToAdd] == buttonSequence[i - 1])
+            {
+                valueToAdd = Random.Range(0, possibleInputs.Count);
+            }
+            
+            buttonSequence.Add(possibleInputs[valueToAdd]);
+        }
+    }
+    private void PressButtonCheck()
+    {
+        if (canInput && playing)
+        {
+            if (inputs.ButtonSequenceInputs.InputQ.WasPressedThisFrame())
+            {
+                inputValue = "Q";
+                CompareInputToDesired();
+            }
+            else if (inputs.ButtonSequenceInputs.InputW.WasPressedThisFrame())
+            {
+                inputValue = "W";
+                CompareInputToDesired();
+            }
+            else if (inputs.ButtonSequenceInputs.InputE.WasPressedThisFrame())
+            {
+                inputValue = "E";
+                CompareInputToDesired();
+            }
+            else if (inputs.ButtonSequenceInputs.InputA.WasPressedThisFrame())
+            {
+                inputValue = "A";
+                CompareInputToDesired();
+            }
+            else if (inputs.ButtonSequenceInputs.InputS.WasPressedThisFrame())
+            {
+                inputValue = "S";
+                CompareInputToDesired();
+            }
+            else if (inputs.ButtonSequenceInputs.InputD.WasPressedThisFrame())
+            {
+                inputValue = "D";
+                CompareInputToDesired();
+            }
+        }
+    }
+    private void CompareInputToDesired()
+    {
+        if (playing)
+        {
+            if (inputValue != desiredValue && inputValue != "Null")
+            {
+                canInput = false;
+                Debug.Log("WrongButton");
+                Invoke("IncorrectButton", waitTime);
+            }
+            else if (inputValue == desiredValue)
+            {
+                if (currKeyNum == buttonSequence.Count - 1)
+                {
+                    Debug.Log("Success");
+
+                    if (looping && numberOfLoops != 1)
+                    {
+                        GenerateSequence();
+                        numberOfLoops--;
+                        return;
+                    } else if (looping && numberOfLoops == 1)
+                    {
+                        Debug.Log("Finished Loop");
+                    }
+
+                    playing = false;
+                    return;
+
+                } else
+                {
+                    currKeyNum++;
+                }
+
+                desiredValue = buttonSequence[currKeyNum];
+                Debug.Log("CorrectButton");
+            }
+            else if (inputValue == "Null")
+            {
+                return;
+            }
+        }
+    }
+    private void IncorrectButton()
+    {
+        canInput = true;
+        Debug.Log("Cooldown Finished");
+    }
+
+}
