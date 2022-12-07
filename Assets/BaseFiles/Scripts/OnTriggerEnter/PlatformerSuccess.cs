a99758ca08bcaccfa0696dde11645ca5c371e59f
AndrewT <1026409@student.sae.edu.au>
2022-12-07 10:09:01 +0800

    Scenes 1.5 and 3.0 are working

--- Assets/BaseFiles/Scripts/OnTriggerEnter/PlatformerSuccess.cs
+++ Assets/BaseFiles/Scripts/OnTriggerEnter/PlatformerSuccess.cs
@@ -6,21 +6,8 @@ using UnityEngine.Events;
 
 public class PlatformerSuccess : MonoBehaviour
 {
-
     [SerializeField] private UnityEvent SuccessTriggered;
     [SerializeField] private bool hasBeenTriggered = false;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter(Collider other)
     {
